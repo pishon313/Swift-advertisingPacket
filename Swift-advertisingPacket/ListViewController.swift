0ad4bea8996daacbd37a0fd9c5edd653a99a3df7
Sarah <pishon313@gmail.com>
2023-07-26 14:38:35 +0900

    feat: tableViewController & table view 내용 추가 및 정리
    - UITableViewDelegate, UITableViewDataSource

--- Swift-advertisingPacket/ListViewController.swift
+++ Swift-advertisingPacket/ListViewController.swift
@@ -8,17 +8,20 @@
 import UIKit
 import CoreBluetooth
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // CBCentralManager
     var centralManager: CBCentralManager!
+    var peripherals: [CBPeripheral] = []
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var toggleBLE: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         centralManager = CBCentralManager(delegate: self, queue: nil)
-        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
     @IBAction func toggleBtn(_ sender: Any) {
@@ -26,6 +29,19 @@ class ListViewController: UIViewController {
         if centralManager.state == .poweredOn {
             centralManager.scanForPeripherals(withServices: nil)
         }
+    }
+    
+    // MARK: - TableView Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peripherals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let peripheral = peripherals[indexPath.row]
+        cell.textLabel?.text = peripheral.name ?? "Unknown Device"
+        
+        return cell
     }
     
 }
@@ -46,9 +62,16 @@ extension ListViewController: CBCentralManagerDelegate {
             print("central state is POWERED OFF")
         case .poweredOn:
             print("central state is POWER ON")
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
         default:
             print("central state is default unknown")
         }
-        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !peripherals.contains(peripheral) {
+            peripherals.append(peripheral)
+            tableView.reloadData()
+        }
     }
 }
--- Swift-advertisingPacket/ViewController.swift
+++ Swift-advertisingPacket/ViewController.swift
@@ -9,38 +9,28 @@ import UIKit
 import CoreBluetooth
 
 class ViewController: UIViewController {
-    
-    var centralManager: CBCentralManager!
 
+    @IBOutlet weak var scanBtn: UIButton!
+    @IBOutlet weak var advertisingBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        centralManager = CBCentralManager(delegate: self, queue: nil)
     
     }
-}
-
-extension ViewController:CBCentralManagerDelegate {
     
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    
+    @IBAction func tapScanBtn(_ sender: Any) {
         
-        switch central.state {
-        case .unknown:
-            print("central state is UNKNOWN")
-        case .resetting:
-            print("central state is RESETTING")
-        case .unsupported:
-            print("central state is UNSUPPORTED")
-        case .unauthorized:
-            print("central state is UNAUTHORIZED")
-        case .poweredOff:
-            print("central state is POWERED OFF")
-        case .poweredOn:
-            print("central state is POWER ON")
-        default:
-            print("central state is default unknown")
-        }
-    }
-}
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let listVC = storyboard.instantiateViewController(identifier: "listViewController")
+//        self.present(listVC, animated: true, completion: nil)
+//        self.navigationController?.pushViewController(listVC, animated: true)
 
+    }
+    
+    
+    @IBAction func tapAdvertisingBtn(_ sender: Any) {
+        
+    }
+    
+}
